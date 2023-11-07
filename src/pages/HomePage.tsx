0b3dbb806308f62fb499f70f3ace835049f43c33
mohammad.sohail <mohammad.sohail@appinventiv.com>
2023-11-07 21:40:14 +0530

    genreList added

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,10 +7,14 @@ import GameGrid from '../components/GameGrid'
 const HomePage = () => {
     return (
         <>
-            <GridItem px="5px" area={'sideBar'}>
+            <GridItem
+                display={['none', 'none', 'block']}
+                px="5px"
+                area={'sideBar'}
+            >
                 <GenreList />
             </GridItem>
-            <GridItem px={'2'} area={'main'}>
+            <GridItem px={'4'} area={'main'}>
                 <HStack mb={'4'} spacing={'4'} alignItems={'center'}>
                     <PlatformSelector />
                     <SortSelector />
